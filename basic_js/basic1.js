1a53a2c1e234f9ea8baba92068dd5cd063047f3d
Yunus <yunus.bahar86@gmail.com>
2023-05-05 05:27:56 +0700

    basic js

---
+++ basic_js/basic1.js
@@ -0,0 +1,30 @@
+//Number data type
+let a=4
+console.log(a)
+console.log(typeof(a))
+
+
+//String data type
+let b = "Yunus bahar"
+console.log(b)
+console.log(typeof(b))
+
+let c = a+b
+console.log(c)
+console.log(typeof(c))
+
+const d = 5
+console.log(d)
+
+let e = 5
+e = 6
+console.log(e)
+
+var f = 5
+f = "abc"
+console.log(f)
+
+//cannot redeclare variable with let but possible with var
+//cont -> value cannot be change or 
+//let -> used to reassign value to variable
+//var -> cannot be used to redeclared variable
---
+++ basic_js/basic2.js
@@ -0,0 +1,26 @@
+const flag = false
+
+if(flag)
+{
+    console.log(flag)
+}
+else
+{
+    console.log(flag)
+}
+
+
+let i = 0
+while(i<10)
+{
+    i++
+    console.log(i + "i am inside loop")
+}
+
+let j = 0
+do
+{
+    j++
+    console.log(j)
+}
+while(j<10)
